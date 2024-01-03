5bc9d6d7c2e590613544f5fb614dbc99760bae35
Rajnish Pandey <piyushwinner1997@gmail.com>
2024-01-03 14:21:53 +0530

    create frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Full stack app</h1>
+    </>
+  )
+}
+
+export default App
