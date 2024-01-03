ac88b07c4e1b4c65f60526188be84a55581d081c
Rajnish Pandey <piyushwinner1997@gmail.com>
2024-01-03 16:46:19 +0530

    install axios & API integration for jokes in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/jokes")
+      .then((response) => {
+        setJokes(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <>
       <h1>Full stack app</h1>
+      <p>JOKES: {jokes.length}</p>
+
+      {jokes.map((joke, index) => {
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>;
+      })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
